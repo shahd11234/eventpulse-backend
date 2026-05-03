627da90611abdf51eee59c6c4d4a994bc66633fd
Shahad <20220002585@students.cud.ac.ae>
2026-05-03 21:45:39 +0400

    Fix: remove dotenv completely, Render provides env vars natively

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-if (process.env.NODE_ENV !== 'production') {
-  try { require('dotenv').config(); } catch (e) {}
-}
 
 const app = express();
 
